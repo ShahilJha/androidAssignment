0290a0b14f08b8dbff41664b02855aa54fd8ae0c
Erlen <shahil1.jha@gmail.com>
2020-01-04 16:40:14 +0545

    add fragments for DashboardActivity.java

---
+++ app/src/main/java/com/example/bim5thsem/home/CategoryDashboardFragment.java
@@ -0,0 +1,32 @@
+package com.example.bim5thsem.home;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.bim5thsem.R;
+
+
+public class CategoryDashboardFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_category_dashboard, container, false);
+    }
+
+
+}
