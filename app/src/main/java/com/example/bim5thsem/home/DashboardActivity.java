eafd7f39e02200a406226848cde129910825be33
Erlen <shahil1.jha@gmail.com>
2020-01-04 18:04:25 +0545

    refactored some the icons name

--- app/src/main/java/com/example/bim5thsem/home/DashboardActivity.java
+++ app/src/main/java/com/example/bim5thsem/home/DashboardActivity.java
@@ -1,16 +1,81 @@
 package com.example.bim5thsem.home;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.example.bim5thsem.R;
+import com.google.android.material.tabs.TabLayout;
 
 public class DashboardActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+
+    private int[] tabIcons = {
+            R.drawable.,
+            R.drawable.,
+            R.drawable.
+    };
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        if (item.getItemId() == android.R.id.home){
+            onBackPressed();
+            return true;
+        }
+        else
+            return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater =  getMenuInflater();
+        inflater.inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        iniToolbar();
+
+        viewPager = findViewById(R.id.viewPager);
+        tabLayout = findViewById(R.id.lytTab);
+
+        setupViewPager(viewPager);
+        setTabWithIcon();
+    }
+
+    private void setTabWithIcon(){
+        tabLayout.getTabAt(0).setIcon(tabIcons[0]);
+        tabLayout.getTabAt(1).setIcon(tabIcons[1]);
+        tabLayout.getTabAt(2).setIcon(tabIcons[2]);
+    }
+
+    private void setupViewPager(ViewPager viewPager){
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        adapter.addFragment(new CategoryDashboardFragment(),"Category");
+        adapter.addFragment(new MainDashboardFragment(), "Home");
+        adapter.addFragment(new ProfileDashboardFragment(),"Profile");
+
+        viewPager.setAdapter(adapter);
+    }
+
+    private void iniToolbar(){
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Home");
     }
 }
