2d677a4b36dfeded8b8b262e53bdaec6255cd9a7
ShahilJha <shahil1.jha@gmail.com>
2020-01-18 19:21:44 +0545

    starting call permission

--- app/src/main/java/com/example/bim5thsem/SignupActivity.java
+++ app/src/main/java/com/example/bim5thsem/SignupActivity.java
@@ -1,10 +1,14 @@
 package com.example.bim5thsem;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.Menu;
@@ -13,13 +17,23 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class SignupActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText userName, mailAddress, password, cPassword;
-    private Button signUpBtn;
+    private Button signUpBtn, chooseImage;
     private Toolbar toolbar;
+    private Dialog originalDialog;
+    private AlertDialog.Builder alertDialogBuilder;
+    private ImageView imgPhoto;
+    private Uri picUri;
+    private String currentImagePath;
+    private Bitmap bitmap;
+    private String type = "";
+
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item){
@@ -49,8 +63,11 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
         mailAddress = findViewById(R.id.eMail);
         password = findViewById(R.id.passwordText);
         cPassword = findViewById(R.id.confirmPasswordText);
-        signUpBtn = findViewById(R.id.signupId);
 
+        chooseImage = findViewById(R.id.chooseImage);
+        chooseImage.setOnClickListener(this);
+
+        signUpBtn = findViewById(R.id.signupId);
         signUpBtn.setOnClickListener(this);
     }
 
@@ -64,6 +81,10 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
     @Override
     public void onClick(View v) {
         switch(v.getId()){
+            case R.id.chooseImage:
+                selectImage();
+                break;
+
             case R.id.signupId:
                 if (TextUtils.isEmpty(userName.getText()) || TextUtils.isEmpty(mailAddress.getText()) || TextUtils.isEmpty(password.getText()) || TextUtils.isEmpty(cPassword.getText())){
                     Toast.makeText(getApplicationContext(), "Please fill up all spaces", Toast.LENGTH_SHORT).show();
@@ -84,5 +105,40 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
             default:
                 break;
         }
+    }
+
+    private void selectImage() {
+        alertDialogBuilder = new AlertDialog.Builder(this);
+        View dialogView = View.inflate(this, R.layout.upload_photo, null);
+        TextView txtTakePhoto = dialogView.findViewById(R.id.txtTakePhoto);
+        TextView txtChooseGallery = dialogView.findViewById(R.id.txtChooseGallery);
+        TextView txtCancel = dialogView.findViewById(R.id.txtCancel);
+        alertDialogBuilder.setView(dialogView);
+        txtTakePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                type = "Camera";
+                originalDialog.dismiss();
+                openCamera();
+            }
+        });
+        txtChooseGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                type = "Gallery";
+                originalDialog.dismiss();
+                openGallery();
+            }
+        });
+        originalDialog = alertDialogBuilder.create();
+        txtCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                originalDialog.dismiss();
+            }
+        });
+        originalDialog.getWindow().setDimAmount(0.7f);
+        originalDialog.setCanceledOnTouchOutside(true);
+        originalDialog.show();
     }
 }
