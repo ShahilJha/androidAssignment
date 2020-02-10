5725d2ef1f6346c2ee696c89703fddd45097532a
ShahilJha <shahil1.jha@gmail.com>
2020-02-10 19:00:56 +0545

    added the User.java file for database

---
+++ app/src/main/java/com/example/bim5thsem/User.java
@@ -0,0 +1,44 @@
+package com.example.bim5thsem;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+public class User extends RealmObject {
+    @PrimaryKey
+    private  int id;
+    private String username;
+    private String email;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
